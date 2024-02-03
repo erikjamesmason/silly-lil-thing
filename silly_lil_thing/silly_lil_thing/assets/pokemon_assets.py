6538f58284efa486876ee6875d3fd50897244148
erikjamesmason <ejmdpg@gmail.com>
2024-02-02 23:49:01 -0800

    working change and also req txt for env

--- silly_lil_thing/silly_lil_thing/assets/pokemon_assets.py
+++ silly_lil_thing/silly_lil_thing/assets/pokemon_assets.py
@@ -1,91 +1,84 @@
 import pandas as pd
-import requests 
-import io 
+import requests
+import io
 from dagster_gcp import BigQueryResource
 from dagster_duckdb import DuckDBResource
 
 from dagster import (
-    asset,
-    get_dagster_logger,
-    AssetExecutionContext
+    asset, 
+    get_dagster_logger, 
+    AssetExecutionContext,
+    MetadataValue
 )
 
 logger = get_dagster_logger()
 
-@asset(
-    group_name='pokemon',
-    compute_kind='Response'
-)
-def pokemon(context: AssetExecutionContext)->requests.Response:
-    """
-    """
-    pokemon_response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=2000')
+
+@asset(group_name="pokemon", compute_kind="Response")
+def pokemon(context: AssetExecutionContext) -> requests.Response:
+    """ """
+    pokemon_response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=2000")
 
     context.add_output_metadata(
-        metadata={
-            "status": pokemon_response.status_code,
-            "url": pokemon_response.url
-        },
+        metadata={"status": pokemon_response.status_code, "url": pokemon_response.url},
     )
 
     return pokemon_response
 
-@asset(
-    group_name='pokemon',
-    compute_kind='Response'
-)
-def pokemon_types(context: AssetExecutionContext)->requests.Response:
-    """
-    """
-    response_types = requests.get('https://pokeapi.co/api/v2/type')
+
+@asset(group_name="pokemon", compute_kind="Response")
+def pokemon_types(context: AssetExecutionContext) -> requests.Response:
+    """ """
+    response_types = requests.get("https://pokeapi.co/api/v2/type")
 
     return response_types
 
-@asset(
-    group_name='pokemon',
-    compute_kind='Pandas'
-)
-def pokemon_df(context: AssetExecutionContext, pokemon)->pd.DataFrame:
-    """
-    """
-    pokemon_df = pd.json_normalize(pokemon)
+
+@asset(group_name="pokemon", compute_kind="Pandas")
+def pokemon_df(context: AssetExecutionContext, pokemon: requests.Response) -> pd.DataFrame:
+    """ """
+    pokemon_df = pd.json_normalize(pokemon.json()['results'])
 
     context.add_output_metadata(
         metadata={
-            "dataframe shape": pokemon_df.shape[0],
-            "dataframe schema": str(pokemon_df.info())
+            "dataframe shape": list(pokemon_df.shape),
+            "preview":  MetadataValue.md(pokemon_df.head().to_markdown()),
+            "dataframe schema": str(pokemon_df.columns),
         },
     )
 
     return pokemon_df
 
-@asset(
-    group_name='pokemon',
-    compute_kind='Pandas'
-)
-def pokemon_types_df(context: AssetExecutionContext, pokemon_types)->pd.DataFrame:
-    """
-    """
-    pokemon_types_df = pd.json_normalize(pokemon_types)
+
+@asset(group_name="pokemon", compute_kind="Pandas")
+def pokemon_types_df(context: AssetExecutionContext, pokemon_types:requests.Response) -> pd.DataFrame:
+    """ """
+    pokemon_types_df = pd.json_normalize(pokemon_types.json()['results'])
 
     context.add_output_metadata(
         metadata={
-            "dataframe shape": pokemon_types_df.shape[0],
-            "dataframe schema": pokemon_types_df.info()
+            "dataframe shape": list(pokemon_types_df.shape),
+            "dataframe schema": str(pokemon_types_df.columns),
         },
     )
 
     return pokemon_types_df
 
-@asset(
-    deps=[pokemon_df, pokemon_types_df],
-    group_name='pokemon',
-    compute_kind='DuckDB'
-)
-def pokemon_data(context: AssetExecutionContext, duckdb: DuckDBResource):
 
+@asset(group_name="pokemon", compute_kind="DuckDB")
+def pokemon_data(
+    context: AssetExecutionContext,
+    pokemon_df: pd.DataFrame,
+    pokemon_types_df:pd.DataFrame,
+    duckdb: DuckDBResource,
+):
+    
+    pokemon_df = pokemon_df
+    pokemon_types_df = pokemon_types_df
+
+    logger.info(f"type is {type(pokemon_df)}")
     with duckdb.get_connection() as duck_conn:
-        duck_conn.execute(
+        duck_conn.query(
             """create or replace table pokemon 
              as (
                     select * from pokemon_df)
